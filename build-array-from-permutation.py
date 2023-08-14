31965d4a0894d52735f0836fde195e7894962b49
Gokulakrishnan <gokulakrishnanmay29@gmail.com>
2023-08-14 15:47:29 +0530

    https://leetcode.com/problems/build-array-from-permutation

---
+++ build-array-from-permutation.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[nums[dict(zip(nums, range(len(nums))))[i]]] for i in nums]
+
+
+sol = Solution()
+print(sol.buildArray([0, 2, 1, 5, 3, 4]))
