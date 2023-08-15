b20285fb3a823af654b9e9b12c264dd3265ee80d
Gokulakrishnan <gokulakrishnan.work@gmail.com>
2023-08-15 19:06:24 +0530

    longest-substring-without-repeating-characters

---
+++ longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+# cant solve on own
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        return s
+
+
+sol = Solution()
+print(sol.lengthOfLongestSubstring("abcabcbb"))  # 3
+print(sol.lengthOfLongestSubstring("bbbbb"))  # 1
+print(sol.lengthOfLongestSubstring("pwwkew"))  # 3
+print(sol.lengthOfLongestSubstring("x"))  # 1
+print(sol.lengthOfLongestSubstring("dvdf"))  # 3
